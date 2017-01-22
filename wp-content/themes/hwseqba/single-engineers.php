346afb927d26104cc695d35c3bbd2b63a939bc1e
Michael Buxton <mjmb@btinternet.com>
2017-01-22 12:35:44 +0000

    Mobile nav and engineers mobile

--- wp-content/themes/hwseqba/single-engineers.php
+++ wp-content/themes/hwseqba/single-engineers.php
@@ -12,11 +12,10 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main" style="margin-bottom: -6px;">
 <!-- Header -->
-        <div id="rooms-header">
-            <div id="home-header" style="background: url('<?php the_field('clients_background', 'option'); ?>') fixed;">
-                <div class="header-content-bg">
-                </div>
-                <div class="header-content" style="border-bottom: white 3px solid;">
+        <div id="engineer-header">
+            <div id="home-header">
+                <div class="header-content">
+										<img class="" src="<?php the_field('engineer_image'); ?>">
                     <h1><span><?php the_title(); ?></span></h1>
                     <p class="wide-paragraph"><?php the_field('brief_bio'); ?></p>
                 </div>
@@ -26,14 +25,8 @@ get_header(); ?>
 
 <!-- Start the Loop. -->
 			<div class="engineers-container">
-				<div class="card">
-					<div class="column-left">
-          	<img src="<?php the_field('engineer_image'); ?>">
-					</div>
-					<div class="column-right">
-						<?php the_content() ?>
-					</div>
-				</div>
+				<?php the_content() ?>
+				<p><a href="<?php the_permalink();?>" class="CTA">Back to engineers</a></p>
 			</div>
 			<?php wp_reset_query(); ?>
 		</main><!-- #main -->
